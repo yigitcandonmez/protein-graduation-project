eacfa8081cd19f037493a2542027f7bf0e01581c
yigitcandonmez <muratcandonmez123@gmail.com>
2022-05-01 06:19:03 +0300

    react-helmet construct instead of useDocumentTitle hook

--- src/hooks/useDocumentTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useDocumentTitle = (initialValue = '') => {
-	useEffect(() => {
-		document.title = `İkinci El Project | ${initialValue}`;
-	}, [initialValue]);
-};
-
-export default useDocumentTitle;
