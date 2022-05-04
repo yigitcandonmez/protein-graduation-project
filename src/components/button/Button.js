562307f5a66de32a871653288266ffcb74ddc0e3
yigitcandonmez <muratcandonmez123@gmail.com>
2022-05-04 04:14:21 +0300

    add new prop named id

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,11 +2,12 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './Button.module.css';
 
-function Button({ label, primary, className, handleClick }) {
+function Button({ label, id, primary, className, handleClick }) {
 	const mode = primary ? 'primary' : 'secondary';
 
 	return (
 		<button
+			id={id}
 			type="submit"
 			onClick={handleClick}
 			className={cx(styles['button-submit'], styles[`button-${mode}`], className)}
