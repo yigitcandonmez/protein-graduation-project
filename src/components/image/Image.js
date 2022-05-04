b1dde32317015b2832fad51daa8b893b40ebbc42
yigitcandonmez <muratcandonmez123@gmail.com>
2022-05-04 04:14:58 +0300

    add new prop named loading for lazy loading

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -3,12 +3,13 @@ import styles from './Image.module.css';
 
 const BASE_URL = 'https://bootcamp.akbolat.net/';
 
-function Image({ localSrc, src, alt, width, height, className }) {
+function Image({ localSrc, src, alt, loading, width, height, className }) {
 	return (
 		<img
 			src={!localSrc ? `${BASE_URL}${src}` : localSrc}
 			alt={alt}
 			width={width}
+			loading={loading}
 			height={height}
 			className={cx(styles.image, className)}
 		/>
