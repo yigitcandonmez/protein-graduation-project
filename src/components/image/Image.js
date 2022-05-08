780f34c46721b5ad5d74b74e5d0f34d03d9f2f5b
yigitcandonmez <muratcandonmez123@gmail.com>
2022-05-08 15:13:28 +0300

    onLoad to gain performance

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -3,13 +3,14 @@ import styles from './Image.module.css';
 
 const BASE_URL = 'https://bootcamp.akbolat.net/';
 
-function Image({ localSrc, src, alt, loading, width, height, className }) {
+function Image({ localSrc, src, alt, loading, width, height, className, onLoad }) {
 	return (
 		<img
 			src={!localSrc ? `${BASE_URL}${src}` : localSrc}
 			alt={alt}
 			width={width}
 			loading={loading}
+			onLoad={onLoad}
 			height={height}
 			className={cx(styles.image, className)}
 		/>
